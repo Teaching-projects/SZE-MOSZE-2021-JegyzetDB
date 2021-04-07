5480ed2d01a8e73cf95f4668a17a55c7a5f767a0
szotbalint <szotakbalint@gmail.com>
2021-04-07 15:23:34 +0200

    First init commit, set the deafult MVC files and database reg methods

---
+++ Plugin.php
@@ -0,0 +1,14 @@
+<?php namespace Szb\Jegyzet;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+    }
+}
---
+++ updates/builder_table_update_szb_jegyzet_.php
@@ -0,0 +1,25 @@
+<?php namespace Szb\Jegyzet\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateSzbJegyzet extends Migration
+{
+    public function up()
+    {
+        Schema::table('szb_jegyzet_', function($table)
+        {
+            $table->date('created_at');
+            $table->date('updated_at');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('szb_jegyzet_', function($table)
+        {
+            $table->dropColumn('created_at');
+            $table->dropColumn('updated_at');
+        });
+    }
+}
