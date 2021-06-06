9190a2adf43299af4f9c9bee0a98f276469bfbdd
szotbalint <szotakbalint@gmail.com>
2021-06-06 22:11:52 +0200

    jegyzet MVC backend

--- controllers/NoteContoller.php
+++ controllers/NoteContoller.php
@@ -1,16 +1,30 @@
 <?php namespace Szb\Jegyzet\Controllers;
 
+/**
+*\Brief 
+*Jegyzet Controller
+*
+*\class Controller, Backendmanu
+*
+*/
+
 use Backend\Classes\Controller;
 use BackendMenu;
 
+
 class NoteContoller extends Controller
 {
-    public $implement = [        'Backend\Behaviors\ListController',        'Backend\Behaviors\FormController',        'Backend\Behaviors\ReorderController'    ];
+    public $implement = [        'Backend\Behaviors\ListController',        'Backend\Behaviors\FormController',        'Backend\Behaviors\ReorderController'    ]; 
+    /*!< implemnt backend menu options in CMS */
     
-    public $listConfig = 'config_list.yaml';
-    public $formConfig = 'config_form.yaml';
-    public $reorderConfig = 'config_reorder.yaml';
+    public $listConfig = 'config_list.yaml'; /*!< config list form list model data in CMS */
+    public $formConfig = 'config_form.yaml'; /*!< config the form CRUD options in CMS */
+    public $reorderConfig = 'config_reorder.yaml'; /*!< Record config options */
 
+    /** \brief constructor for set backend manu URL and backend CMS area
+      * \param none
+      * \return none
+    */
     public function __construct()
     {
         parent::__construct();
--- models/Note.php
+++ models/Note.php
@@ -3,25 +3,37 @@
 use Model;
 
 /**
- * Model
- */
+*\Brief 
+*Note model
+*
+*\class Model
+*/
 class Note extends Model
 {
-    use \October\Rain\Database\Traits\Validation;
+    use \October\Rain\Database\Traits\Validation;  /*!< namespace for Validation */
     
-    use \October\Rain\Database\Traits\SoftDelete;
+    use \October\Rain\Database\Traits\SoftDelete; /*!< Set bacause use SoftDelete support in CMS*/
 
-    protected $dates = ['deleted_at'];
+    protected $dates = ['deleted_at'];  /*!< Set bacause use SoftDelete support in CMS*/
 
 
     /**
      * @var string The database table used by the model.
      */
-    public $table = 'szb_jegyzet_';
+    public $table = 'szb_jegyzet_'; /*!< table for this component */
 
     /**
      * @var array Validation rules
      */
     public $rules = [
     ];
+	
+    /*!< File attachments - polymorphic relationship.  */
+	public $attachOne = [
+    'pdf_file' => 'System\Models\File',
+    'image' => 'System\Models\File',
+	];
+	
+
+	
 }
